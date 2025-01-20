16fcf9988fc7395674840b2e00d8b7d5d2a28a33
Apur0y <apuroy2785@gmail.com>
2025-01-20 19:43:43 +0600

    authenicatino

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { router } from './Routes/Routes'
-import { RouterProvider } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { router } from "./Routes/Routes";
+import { RouterProvider } from "react-router-dom";
+import AuthProvider from "./Providers/AuthProvider";
 
-
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-   <div className='max-w-screen-2xl mx-auto'>
-   <RouterProvider router={router} />
-   </div>
-    </StrictMode>,
-)
+    <AuthProvider>
+      <div className="max-w-screen-2xl mx-auto">
+        <RouterProvider router={router} />
+      </div>
+    </AuthProvider>
+  </StrictMode>
+);
